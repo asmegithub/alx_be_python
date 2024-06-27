8e0f6379982a85f1acb688052f416eb419d3697f
Asmare Zelalem <asmarestu12@gmail.com>
2024-06-27 13:04:22 +0300

    fixing reminder script based on the  required patteren

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,14 +1,15 @@
-task=input("What would you like me to remind you about? ")
-priority=input("What is the priority of this task? (high, medium, low) ")
-time_bounded=input("Is this time bounded? (yes, no) ")
+task = input("Enter your task: ")
+time_bounded = input("Is it time-bound? (yes/no): ")
+priority = input("Priority (high/medium/low): ")
+
 match time_bounded:
     case "yes":
-        if(priority=="high"):
+        if priority == "high":
             print(f"Reminder: {task}! is a high priority task that requires immediate attention today!")
         else:
-            print(f"Note: {task}! is a low priority task that requires immediate attention today! ")
+            print(f"Note: {task}! is a low priority task that requires immediate attention today!")
     case "no":
-        if(priority=='high'):
-            print(f"Reminder: {task}! is a high priority task Consider completing it when you have free time.")
+        if priority == "high":
+            print(f"Reminder: {task}! is a high priority task. Consider completing it when you have free time.")
         else:
-            print(f"Note: {task}! is a low priority task Consider completing it when you have free time.")
+            print(f"Note: {task}! is a low priority task. Consider completing it when you have free time.")
