692a06e612ef4ca169533336885b5b131ba161ff
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-08 12:04:04 +0300

    funs_and_dsa task0

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1, num2, operation):
+    result
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "You can't divide by zero"
+            else:
+                result = num1 / num2
+        case _:
+            print("Invalid operation")
+    return result
