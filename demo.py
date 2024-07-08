772da372bc5ad0ea7e27a5cf9e073882ddb75cea
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-08 21:25:40 +0300

    week-5 task2

--- demo.py
+++ demo.py
@@ -1,51 +1,5 @@
-shopping_list = []
+import datetime
 
-
-def add(item):
-    global shopping_list
-    shopping_list.append(item)
-
-
-def remove(item):
-    global shopping_list
-    if item in shopping_list:
-        shopping_list.remove(item)
-    else:
-        print("Item not found")
-
-
-def view():
-    print(shopping_list)
-
-
-def display_menu():
-    print("Shopping List Manager")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
-
-
-def main():
-    while True:
-        display_menu()
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            add(input("Enter item: "))
-            pass
-        elif choice == "2":
-            remove(input("Enter item: "))
-            pass
-        elif choice == "3":
-            view()
-            pass
-        elif choice == "4":
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-
-if __name__ == "__main__":
-    main()
+num_days = int(input("Enter the number of days: "))
+future_date = datetime.datetime.now() + datetime.timedelta(days=num_days)
+print(future_date.strftime("%Y-%m-%d"))
