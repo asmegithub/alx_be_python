40cf8ed4b09fcce6a98e29b99bc4e82289990df1
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-09 07:39:13 +0300

    tring to fix bugs

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -10,18 +10,22 @@ def convert_to_fahrenheit(celsius):
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
 
-input_temp = input("Enter the temperature to convert:")
+input_temp = input("Enter the temperature to convert: ")
 try:
     input_temp = float(input_temp)
 except ValueError:
     print("Invalid input. Please enter a valid number.")
     exit()
 
-choice = input("Is this temperature in Celsius or Fahrenheit? (C/F):")
-match choice.capitalize():
+choice = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").capitalize()
+match choice:
     case "F":
-        print(convert_to_celsius(input_temp))
+        print(
+            f"{input_temp} degrees Fahrenheit is {convert_to_celsius(input_temp)} degrees Celsius."
+        )
     case "C":
-        print(convert_to_fahrenheit(input_temp))
+        print(
+            f"{input_temp} degrees Celsius is {convert_to_fahrenheit(input_temp)} degrees Fahrenheit."
+        )
     case _:
         print("Invalid choice. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
