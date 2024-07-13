4a1630bb45454448f1162c99e3d80ca0971456ea
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-13 23:17:30 +0300

    chore: Refactor SimpleCalculator class and test cases

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -21,3 +21,36 @@ class SimpleCalculator:
         if b == 0:
             return None
         return a / b
+
+
+class SimpleCalculatorTest(unittest.TestCase):
+    def test_add(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.add(1, 2), 3)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.subtract(1, 2), -1)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(0, 0), 0)
+
+    def test_multiply(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.multiply(1, 2), 2)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(0, 0), 0)
+
+    def test_divide(self):
+        calc = SimpleCalculator()
+        self.assertEqual(calc.divide(1, 2), 0.5)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(0, 1), 0)
+        self.assertIsNone(calc.divide(1, 0))
+        self.assertIsNone(calc.divide(0, 0))
+    pass
+
+
+# if __name__ == "__main__":
+#     unittest.main()
