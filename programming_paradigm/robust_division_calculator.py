cee3ed1a08e864dc751ad6bea02ccd270c076350
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-13 22:25:02 +0300

    task1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,3 @@
-class ZeroDivisionError(Exception):
-    def __str__(self):
-        print("Error: Cannot divide by zero.")
-
 
 def safe_divide(numerator, denominator):
     try:
@@ -12,4 +8,4 @@ def safe_divide(numerator, denominator):
     except ValueError:
         raise ValueError("Error: Please enter numeric values only.") from None
     except ZeroDivisionError:
-        raise ZeroDivisionError
+        raise ZeroDivisionError("Error: Cannot divide by zero.")
