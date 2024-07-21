e3cbb90dc1435255622aa453b88e12d2235be8d9
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-21 23:47:05 +0300

    Refactor file size unit in EBook print statement

--- oop/library_system.py
+++ oop/library_system.py
@@ -33,7 +33,7 @@ class Library:
         for book in self.books:
             if isinstance(book, EBook):
                 print(
-                    f"EBook: {book.title} by {book.author}, File Size: {book.file_size} MB")
+                    f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(
                     f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
