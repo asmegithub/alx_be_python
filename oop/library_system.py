1ec385d60bcb38c0d0934b1897d6a941d898dfe5
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-21 23:44:19 +0300

    Refactor __str__ method in Book class to provide a formatted string representation

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,6 +6,9 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
